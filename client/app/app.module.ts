897f943bffb19c57bb2fa1e0f607a2e0a551666d
Paul Morea <paul_morea@hotmail.fr>
2018-05-04 02:03:31 +0200

    test

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -17,6 +17,7 @@ import { LogoutComponent } from './logout/logout.component';
 import { AccountComponent } from './account/account.component';
 import { AdminComponent } from './admin/admin.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { TestComponent } from './test/test.component';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -33,6 +34,7 @@ export function tokenGetter() {
     AccountComponent,
     AdminComponent,
     NotFoundComponent,
+    TestComponent,
   ],
   imports: [
     RoutingModule,
