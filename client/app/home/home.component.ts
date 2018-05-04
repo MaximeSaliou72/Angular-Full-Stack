13329b0ec85b39a28eb04ea14e7876a3a3b59767
Raguenes Maxime <maxime.raguenes2@gmail.com>
2018-05-04 12:05:09 +0200

    dev done

--- client/app/home/home.component.ts
+++ client/app/home/home.component.ts
@@ -8,6 +8,7 @@ import { ToastComponent } from '../shared/toast/toast.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
 
